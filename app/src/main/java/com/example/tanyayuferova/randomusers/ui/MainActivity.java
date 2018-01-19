7022c42e3393b385cf3925130c65e2d9c256382b
tanya-yu <tanya_yu_95@mail.ru>
2018-01-19 18:12:10 +0500

    Main activity appearance

--- app/src/main/java/com/example/tanyayuferova/randomusers/ui/MainActivity.java
+++ app/src/main/java/com/example/tanyayuferova/randomusers/ui/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, getResources().getInteger(R.integer.users_columns_count)));
         adapter = new UsersAdapter(this);
         recyclerView.setAdapter(adapter);
 
