552de5c6dcd0216e97d37e414609c12eb3401229
tanya-yu <tanya_yu_95@mail.ru>
2018-01-29 23:33:40 +0500

    Details Activity

--- app/src/main/java/com/example/tanyayuferova/randomusers/ui/DetailsActivity.java
+++ app/src/main/java/com/example/tanyayuferova/randomusers/ui/DetailsActivity.java
@@ -1,19 +1,15 @@
 package com.example.tanyayuferova.randomusers.ui;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.EditText;
 
 import com.example.tanyayuferova.randomusers.R;
 import com.example.tanyayuferova.randomusers.databinding.ActivityDetailsBinding;
-import com.example.tanyayuferova.randomusers.emailer.UserInfoEmailSender;
 import com.example.tanyayuferova.randomusers.entity.User;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -38,44 +34,23 @@ public class DetailsActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void emailOnClick(View view) {
+    public void emailBtnOnClick(View view) {
         String email = binding.getUser().getEmail();
-        String subject = "Subject";
-        String content = "Content";
+        String subject = getString(R.string.email_subject);
+        String content = getString(R.string.email_content);
 
         final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
         emailIntent.setType("plain/text");
         emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] { email });
         emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
         emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, content);
-        startActivity(Intent.createChooser(emailIntent, "Send email"));
+        startActivity(Intent.createChooser(emailIntent, getString(R.string.email_send)));
     }
 
-    public void sendUserInfoBtnOnClick(View view) {
-        LayoutInflater li = LayoutInflater.from(this);
-        View promptsView = li.inflate(R.layout.email_dialog, null);
-        AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(this);
-        mDialogBuilder.setView(promptsView);
-        final EditText emailView = (EditText) promptsView.findViewById(R.id.email);
-
-        mDialogBuilder
-                .setCancelable(false)
-                .setPositiveButton("OK",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                String email = emailView.getText().toString();
-                                UserInfoEmailSender sender = new UserInfoEmailSender(DetailsActivity.this, binding.getUser(), email);
-                                sender.execute();
-                            }
-                        })
-                .setNegativeButton("Cancel",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-
-        AlertDialog alertDialog = mDialogBuilder.create();
-        alertDialog.show();
+    public void shareBtnOnClick(View view) {
+        startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText(binding.getUser().getFullName()) //TODO more information
+                .getIntent(), getString(R.string.action_share)));
     }
 }
